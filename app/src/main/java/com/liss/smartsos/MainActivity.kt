ab076f08d454fd46c1481ce7ea79d9a7913ae6df
Cristian Melendez <cdmelendez_@hotmail.com>
2023-05-02 14:37:49 -0700

    Conectividad Bluetooth via Serial y envio de SMS
    - Agregado un chequeo al iniciar la aplicacion solicitando al usuario permisos de SMS
    - Agregado la funcionalidad de enviar SMS, en este momento se envia presionando un boton
    - Agregada la conectividad Bluetooth por medio de serial
    - Agregada la deteccion de pulsacion de boton Bluetooth por medio de serial
    
    Nota: falta agregar la ubicacion del telefono en el mensaje, elegir el numero a enviar el mensaje, obtener la MAC del dispositivo bluetooth automaticamente y mover la funcionalidad de deteccion serial a otra parte ya que actualmente la aplicacion muestra una pantalla negra mientras espera la pulsacion del boton

--- app/src/main/java/com/liss/smartsos/MainActivity.kt
+++ app/src/main/java/com/liss/smartsos/MainActivity.kt
@@ -21,14 +21,16 @@ import androidx.appcompat.app.AppCompatDelegate
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.telephony.gsm.SmsManager
-
-import android.util.Log
-import java.io.IOException
-import java.io.InputStream
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.net.Uri
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
 import java.util.*
-import android.os.Looper
-import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +38,14 @@ class MainActivity : AppCompatActivity() {
 
         //Forzar que el modo oscuro se deshabilite
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
         setContentView(R.layout.activity_main)
+
+        //Solicitar permisos iniciales
+        if (!isSmsPermissionGranted()) {
+            showPermissionDialog()
+        } else {
+            // El permiso ya ha sido concedido
+        }
 
         //Boton de prueba para ejecutar la aplicacion 911
         val buttonTest = findViewById<Button>(R.id.button)
@@ -45,7 +53,41 @@ class MainActivity : AppCompatActivity() {
             //Ejecuta la funcion al pulsar el boton
             //exec911()
             //sendSMS("6645333103", "Mensaje de prueba",this)
-            sendSMS("6643538663", "Mensaje de prueba 123 hola",this)
+            sendSMS("6641873545", "Mensaje de prueba 123 hola",this)
+        }
+
+        //Serial
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            // El dispositivo no admite Bluetooth
+        } else {
+            if (!bluetoothAdapter.isEnabled()) {
+                // El Bluetooth no está activado, se puede solicitar al usuario que lo active
+            } else {
+                // El Bluetooth está activado
+                val device = bluetoothAdapter.getRemoteDevice("98:D3:31:F5:B8:2E")
+                val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+                val socket = device.createRfcommSocketToServiceRecord(uuid)
+                socket.connect()
+
+                val inputStream = socket.inputStream
+                val buffer = ByteArray(1024)
+                var bytes: Int
+
+                while (true) {
+                    bytes = inputStream.read(buffer)
+                    val message = String(buffer, 0, bytes)
+                    runOnUiThread {
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        if (message == "1")
+                        {
+                            exec911()
+                            sendSMS("6642867484", "Mensaje de prueba 123",this)
+                        }
+                    }
+                }
+                socket.close()
+            }
         }
 
         //BT
@@ -64,6 +106,31 @@ class MainActivity : AppCompatActivity() {
         //TEST
     }
 
+    //Solicitando permisos
+    private val PERMISSION_REQUEST_SEND_SMS = 123 // número de solicitud de permiso
+    // Función para verificar si se tiene permiso para enviar SMS
+    private fun isSmsPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Función para solicitar permiso para enviar SMS
+    private fun requestSmsPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), PERMISSION_REQUEST_SEND_SMS)
+    }
+
+    // Función para mostrar un diálogo de permisos
+    private fun showPermissionDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setMessage("Esta aplicación necesita los permisos siguientes para funcionar correctamente.")
+            setPositiveButton("Aceptar") { _, _ ->
+                requestSmsPermission()
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
     //Ejecutar aplicacion 911
     private fun exec911() {
         //Nombre interno de la aplicacion 911
@@ -77,6 +144,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Ejecutar aplicacion MedTrak
     private fun execMedTrack() {
         //Nombre interno de la aplicacion MedTrack
         val launchIntent2 = packageManager.getLaunchIntentForPackage("med.track.med")
@@ -107,11 +175,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //SMS
+    //Enviar SMS
     private fun sendSMS(phoneNumber: String, message: String, context: Context) {
+        // Comprobamos si tenemos permiso para enviar SMS
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Si no tenemos permiso, mostramos un mensaje al usuario
+            Toast.makeText(context, "Se requiere permiso para enviar SMS", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Creamos el PendingIntent con la flag FLAG_IMMUTABLE
         val sentIntent = Intent("SMS_SENT").let { sentIntent ->
             PendingIntent.getBroadcast(context, 0, sentIntent, PendingIntent.FLAG_IMMUTABLE)
         }
+
+        // Enviamos el mensaje
         val smsManager = SmsManager.getDefault()
         smsManager.sendTextMessage(phoneNumber, null, message, sentIntent, null)
     }
